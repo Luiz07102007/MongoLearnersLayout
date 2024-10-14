5459026ebe2fa18fc13e623c822cc7fecad0a8c4
Luiz07102007 <luiz.rodrigues198@etec.sp.gov.br>
2024-10-14 12:59:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mongolearners/CadastroTela.java
@@ -0,0 +1,36 @@
+package com.example.mongolearners;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CadastroTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro_tela);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView irLogin = findViewById(R.id.textView10);
+
+        irLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CadastroTela.this, LoginTela.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
